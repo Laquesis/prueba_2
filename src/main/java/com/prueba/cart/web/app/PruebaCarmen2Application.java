a2ed1fca2f6172ac8e8e37918ddee7fd407b9a13
Laquesis <carmenrag12@gmail.com>
2022-11-26 19:19:12 +0100

    Commit inicial

---
+++ src/main/java/com/prueba/cart/web/app/PruebaCarmen2Application.java
@@ -0,0 +1,13 @@
+package com.prueba.cart.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCarmen2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCarmen2Application.class, args);
+	}
+
+}
